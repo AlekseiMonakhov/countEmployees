24c2b2889ebe6092f8641b486823197e709dda51
Ken-Dra <for.shit@yahoo.com>
2021-12-17 01:58:48 +0300

    add try/catch

--- controller/department.controller.js
+++ controller/department.controller.js
@@ -2,18 +2,31 @@ const db = require('../db')
 
 class DepartmentController {
     async createDepartment (req, res){
+       try {
         const {id, title} = req.body
         const newDepartment = await db.query('INSERT INTO Departments (DepartmentId, DepartmentTitle) values ($1, $2) RETURNING *', [id, title])
         res.json(newDepartment.rows)
-    }
+    }  catch (error) {
+        res.json(error)
+    }            
+    } 
+    
     async getDepartments (req, res){
+       try {
         const departments = await db.query('SELECT * FROM Departments')
         res.json(departments.rows)
+    }  catch (error) {
+        res.json(error)  
+    }
     }
     async updateDepartment (req, res){
-        const {id, title} = req.body
-        const departments = await db.query ('UPDATE Departments set DepartmentTitle = $1 where DepartmentId = $2 RETURNING *', [title, id])
-        res.json(departments.rows)
+        try {
+         const {id, title} = req.body
+         const departments = await db.query ('UPDATE Departments set DepartmentTitle = $1 where DepartmentId = $2 RETURNING *', [title, id])
+         res.json(departments.rows)
+    }   catch (error) {
+        res.json(error)  
+    }
     }
     
 }
--- controller/employee.controller.js
+++ controller/employee.controller.js
@@ -2,20 +2,31 @@ const db = require('../db')
 
 class EmployeeController {
     async createEmployee (req, res){
-        const {id, name, age, department} = req.body
-        const newEmployee = await db.query('INSERT INTO Employees (EmployeeId, EmployeeName, EmployeeAge, DepartmentTitle) values ($1, $2, $3, $4) RETURNING *', [id, name, age, department])
-        res.json(newEmployee.rows)
+        try {
+         const {id, name, age, department} = req.body
+         const newEmployee = await db.query('INSERT INTO Employees (EmployeeId, EmployeeName, EmployeeAge, DepartmentTitle) values ($1, $2, $3, $4) RETURNING *', [id, name, age, department])
+         res.json(newEmployee.rows)
+    }   catch (error) {
+         res.json(error) 
     }
+    }     
     async getEmployees (req, res){
-        const employees = await db.query('SELECT * FROM Employees')
-        res.json(employees.rows)
-
+        try {
+         const employees = await db.query('SELECT * FROM Employees')
+         res.json(employees.rows)
+    }   catch (error) {
+         res.json(error)  
+    }
     }
     async updateEmployee (req, res){
-        const {id, name, age, department} = req.body
-        const employees = await db.query ('UPDATE Employees set EmployeeName = $1, EmployeeAge = $2, DepartmentTitle = $3 where EmployeeId = $4 RETURNING *', [name, age, department, id])
-        res.json(employees.rows)
-    
+        try {
+         const {id, name, age, department} = req.body
+         const employees = await db.query ('UPDATE Employees set EmployeeName = $1, EmployeeAge = $2, DepartmentTitle = $3 where EmployeeId = $4 RETURNING *', [name, age, department, id])
+         res.json(employees.rows)
+    }   catch (error) {
+         res.json(error)
+    }       
+    }
 }
-}
+
 module.exports = new EmployeeController()
